239cb4d6137a7c996300b255d519ccbb1af52691
Sachin Raja <sachinraja2349@gmail.com>
2022-05-25 20:15:20 -0700

    reorganize adapters and tests

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   test: {
     setupFiles: './test/setup.ts',
+    include: ['**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}', '**/test.ts'],
   },
 })
